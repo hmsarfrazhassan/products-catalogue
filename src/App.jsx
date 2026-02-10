8628119364126040025788082aa3cf5d69c5fd2c
hmsarfrazhassan <hmsarfraz.hassan@gmail.com>
2026-02-11 00:45:47 +0500

    testing route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Categories from "./routes/Categories";
 import profileImage from "./assets/images/profile-img.jpg";
+import SubCategories from "./routes/SubCategories";
 function App() {
   return (
     <div className="pt-10">
@@ -12,6 +13,7 @@ function App() {
       <div className="text-center text-3xl font-semibold">@stunningbags</div>
       <Routes>
         <Route path="" element={<Categories />} />
+        <Route path="a" element={<SubCategories />} />
       </Routes>
     </div>
   );
